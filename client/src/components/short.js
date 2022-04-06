34def91cf458e57e57902992686a1fb200043eef
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-05 22:26:55 -0400

    form validation

--- client/src/components/short.js
+++ client/src/components/short.js
@@ -1,76 +1,102 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { validateAlias, validateURL } from '../services/validateURL.js'
+const axios = require('axios');
+
 
 const Short = () => {
     const [form, setForm] = useState({
         long: '',
         short: '',
     });
-    const [complete, setComplete] = useState(false);
+    const [validated, setValidated] = useState({
+        short: '',
+        shortMessage: '',
+        long: '',
+    });
+
 
     const updateForm = (value) => {
         return setForm((prev) => {
             return {...prev, ...value}
         })
     };
-    
+    const updateValidated = (value) => {
+        return setValidated((prev) => {
+            return {...prev, ...value}
+        })
+    };
+
     const onSubmit = async (e) => { 
         e.preventDefault();
         
         const newEarl = {...form};
-
+        let abort = false;
         if (!validateURL(newEarl.long)){
-            window.alert('invalid url: ' + newEarl.long);
-            return;
+            updateValidated({long: 'is-invalid'})
+            abort = true;
         }
-        if (form.short && !validateAlias(newEarl.short)){
-            window.alert('invalid alias: ' + newEarl.short);
-            return;
-        }
+        else updateValidated({long: ''});
 
-        //TODO: change when user accounts are setup
-        let res = await fetch('http://localhost:5000/short/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',            
-            },
-            body: JSON.stringify(newEarl)
+        if (form.short && !validateAlias(newEarl.short)){          
+            updateValidated({
+                short: 'is-invalid',
+                shortMessage: 'Must be 3-20 characters using only Aa-Zz, 0-9, -, _'});        
+            abort = true;
+        }
+        else updateValidated({short: ''});
+
+        if (abort) return;
+
+        axios.post('http://localhost:5000/short/add', newEarl)
+        .then( (res) => {
+            if (res.data.status == 'success'){
+                //do nav to final card
+                setForm({...form, short: res.data.earl});
+            }
+            else if (res.data.status == 'earl_taken'){
+                updateValidated({
+                    short: 'is-invalid',
+                    shortMessage: 'Alias is taken'});
+            }
+                
         })
         .catch(error => {
             window.alert(error);
             return;
         });
+        
 
-        console.log(res);
 
-        setForm({ long: '', short: ''});
+        
     }
 
     return(
         <div class='shadow-sm' style={{maxWidth: '360px', margin: '0 auto', float: 'none'}}>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} class = 'needs-validation' novalidate>
             <div class='form-group p-2'>
-                <label htmlFor='long'>Your URL</label>
+                <label for='long'>Paste your URL</label>
                 <input 
                     type='text'
-                    className='form-control form-control-lg'
+                    className={'form-control form-control-lg ' + validated.long}
                     id='long'
                     value={form.long}
                     onChange={ (e) => updateForm({ long: e.target.value }) }/>
+                    <span class="invalid-feedback">Invalid URL</span> 
             </div>
             <div class='form-group p-2'>
-                <label htmlFor='long'>Customize your link (optional)</label>
-                <div class='input-group'>
+                <label for='short'>Customize your link (optional)</label>
+                <div class='input-group input-group-lg has-validation'>
                     <span class="input-group-text" id="basic-addon1">https://shortearl.com/</span>
                     <input 
                         type='text'
-                        className='form-control form-control-lg'
+                        className={'form-control form-control-lg ' + validated.short}
                         id='short'
-                        aria-describedby="basic-addon1"
                         value={form.short}
-                        onChange={ (e) => updateForm({ short: e.target.value }) }/> 
-                </div>
+                        aria-describedby="basic-addon2"
+                        onChange={ (e) => updateForm({ short: e.target.value }) }/>    
+                        <div class="invalid-feedback">{validated.shortMessage}</div>                                                                                          
+                </div>                
             </div>
             <div className='form-group p-2'>
                 <input
