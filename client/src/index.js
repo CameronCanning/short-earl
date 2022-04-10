5831fecbf0e623e964f39221c5b341229ff4302b
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-10 14:58:35 -0400

    custom variables

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 //<React.StrictMode>
 //</React.StrictMode>
