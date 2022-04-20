99a1121a0cd72d71393654c21a62e8fca5c89cfe
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-19 23:57:49 -0400

    server fixed for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config({path: '../config.env'});
+require('dotenv').config({path: './config.env'});
 const port = process.env.PORT || 5000;
 //const path = require('path');
 app.use(cors());
