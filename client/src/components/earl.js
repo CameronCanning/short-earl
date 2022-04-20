8151af1a881265790fdd98b2445f17fb6722ee79
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-19 21:23:19 -0400

    rename heroku config vars

--- client/src/components/earl.js
+++ client/src/components/earl.js
@@ -29,7 +29,7 @@ const Earl = ({showEarls, setShowEarls, className}) => {
 
     const target = useRef(null);
     
-    const baseURL = process.env.BASE_URL || 'http://localhost:5000/';
+    const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/';
 
     const setValidationByName = (formName, {status, error}) => {
         setValidation((prev) => {
