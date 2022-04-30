514c94619b3937da68902ce4db6bc810e10f61d8
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-29 23:39:57 -0400

    landing page banner: new art and responsive text size

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -5,18 +5,15 @@ import Earl from '../components/earl';
 
 const Home = () => {
     return(
-        <Layout className=''>
-            <div parentclass='bg-white py-3' className='d-flex justify-content-between flex-column-reverse flex-md-row '>
+        <Layout>
+            <div parentclass='bg-white py-3 my-2' className='d-flex justify-content-between flex-column-reverse flex-md-row '>
                 <div className='d-flex flex-shrink-0 my-auto '>
-                    <span className='display fw-bold'>
-                        Short links, 
-                        <br/>
-                        big results
-                        <br/>
+                    <span className='display fw-bold'>Welcome to short earl
+                        <p className='text-muted fs-4 fw-light'>A beautifully simple URL shortener </p>
                     </span>
                 </div>
                 <div>
-                    <img src={require('../assets/images/bitly.png')}/>
+                    <img src={require('../assets/images/earl_home.jpeg')}/>
                 </div>
             </div>
             <Earl parentclass='bg-main flex-grow-1'/>     
